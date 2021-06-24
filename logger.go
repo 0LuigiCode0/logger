e7203f59c7a4cd1134e0174ab3d51f1594efc126
Luigicode <ddd-dddu@yandex.ru>
2021-06-24 13:14:31 +0200

    №

--- logger.go
+++ logger.go
@@ -10,13 +10,14 @@ import (
 	"time"
 )
 
-//Logger класс логгера
+//Logger модель логгера
 type Logger struct {
-	logs map[string]*level
+	logs map[levelKey]*level
 }
 
+//level модель каждого уровня логгера
 type level struct {
-	level      string
+	level      levelKey
 	color      int
 	backColor  int
 	format     string
@@ -24,51 +25,61 @@ type level struct {
 	file       *os.File
 }
 
+type levelKey string
+
+const (
+	LInfo    levelKey = "INFO"
+	LService levelKey = "SERVICE"
+	LWarning levelKey = "WARNING"
+	LError   levelKey = "ERROR"
+	LFatal   levelKey = "FATAL"
+)
+
 //InitLogger иницилезирует логгер
 func InitLogger(pathFile string) *Logger {
 	Log := Logger{
-		logs: make(map[string]*level),
+		logs: make(map[levelKey]*level),
 	}
 	logI := new(level)
-	logI.level = "INFO"
+	logI.level = LInfo
 	logI.color = 251
 	logI.backColor = 233
 	logI.format = logI.initForm("%{color:b}%{color:f}%{color}    %{level} %{color:rf} %{time:2006/01/02--15:04:05} >>> WHERE \n\t%{file:1}\n MSG %{color:r}\"%{message}\"")
 	logI.formatFile = logI.initForm("   %{level} %{time:2006/01/02--15:04:05} >>> WHERE %{file:1} MSG %{message}")
 
 	logS := new(level)
-	logS.level = "SERVICE"
+	logS.level = LService
 	logS.color = 86
 	logS.backColor = 96
 	logS.format = logS.initForm("%{color:f}%{color:b}%{color} %{level} %{color:rf} %{time:2006/01/02--15:04:05} >>> WHERE \n\t%{file:1}\n MSG %{color:r}\"%{message}\"")
 	logS.formatFile = logS.initForm("%{level} %{time:2006/01/02--15:04:05} >>> WHERE %{file:1} MSG %{message}")
 
 	logW := new(level)
-	logW.level = "WARNING"
+	logW.level = LWarning
 	logW.color = 226
 	logW.backColor = 239
 	logW.format = logW.initForm("%{color:f}%{color:b}%{color} %{level} %{color:rf} %{time:2006/01/02--15:04:05} >>> WHERE \n\t%{file:1}\n MSG %{color:r}\"%{message}\"")
 	logW.formatFile = logW.initForm("%{level} %{time:2006/01/02--15:04:05} >>> WHERE %{file:1} MSG %{message}")
 
 	logE := new(level)
-	logE.level = "ERROR"
+	logE.level = LError
 	logE.color = 9
 	logE.backColor = 188
 	logE.format = logE.initForm("%{color:f}%{color:b}%{color}   %{level} %{color:rf} %{time:2006/01/02--15:04:05} >>> WHERE \n\t%{file}\n MSG %{color:r}\"%{message}\"")
 	logE.formatFile = logE.initForm("  %{level} %{time:2006/01/02--15:04:05} >>> WHERE %{file} MSG %{message}")
 
 	logF := new(level)
-	logF.level = "FATAL"
+	logF.level = LFatal
 	logF.color = 128
 	logF.backColor = 215
 	logF.format = logF.initForm("%{color:f}%{color:b}%{color}   %{level} %{color:rf} %{time:2006/01/02--15:04:05} >>> WHERE \n\t%{file}\n MSG %{color:r}\"%{message}\"")
 	logF.formatFile = logF.initForm("  %{level} %{time:2006/01/02--15:04:05} >>> WHERE %{file} MSG %{message}")
 
-	Log.logs["info"] = logI
-	Log.logs["warn"] = logW
-	Log.logs["err"] = logE
-	Log.logs["fatal"] = logF
-	Log.logs["serv"] = logS
+	Log.logs[LInfo] = logI
+	Log.logs[LService] = logS
+	Log.logs[LWarning] = logW
+	Log.logs[LError] = logE
+	Log.logs[LFatal] = logF
 
 	if pathFile != "" {
 		outFile, err := os.OpenFile(pathFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0755)
@@ -86,27 +97,27 @@ func InitLogger(pathFile string) *Logger {
 	return &Log
 }
 
-//Infof выводит информационное сообщение
+//Infof выводит форматируемое информационное сообщение
 func (l *Logger) Infof(format string, args ...interface{}) {
 	l.logs["info"].log(format, args...)
 }
 
-//Servicef выводит серверное сообщение
+//Servicef выводит форматируемое серверное сообщение
 func (l *Logger) Servicef(format string, args ...interface{}) {
 	l.logs["serv"].log(format, args...)
 }
 
-//Warningf выводит предупреждающее сообщение
+//Warningf выводит форматируемое предупреждающее сообщение
 func (l *Logger) Warningf(format string, args ...interface{}) {
 	l.logs["warn"].log(format, args...)
 }
 
-//Errorf выводит сообщение ошибки
+//Errorf выводит форматируемое сообщение ошибки
 func (l *Logger) Errorf(format string, args ...interface{}) {
 	l.logs["err"].log(format, args...)
 }
 
-//Fatalf выводит критическое сообщение
+//Fatalf выводит форматируемое критическое сообщение и завершает программу
 func (l *Logger) Fatalf(format string, args ...interface{}) {
 	l.logs["fatal"].log(format, args...)
 	os.Exit(1)
@@ -132,12 +143,13 @@ func (l *Logger) Error(args ...interface{}) {
 	l.logs["err"].log("", args...)
 }
 
-//Fatal выводит критическое сообщение
+//Fatal выводит критическое сообщение и завершает программу
 func (l *Logger) Fatal(args ...interface{}) {
 	l.logs["fatal"].log("", args...)
 	os.Exit(1)
 }
 
+//log оснавная функция вывода сообщения в консоль с учетом форматирования
 func (lv *level) log(format string, args ...interface{}) {
 	var message string
 	if strings.TrimSpace(format) != "" {
@@ -162,8 +174,8 @@ func (lv *level) log(format string, args ...interface{}) {
 	}
 }
 
-//SetLevelFormatConsole изменение формата вывода в консоль
-func (l *Logger) SetLevelFormatConsole(level, format string) {
+//SetLevelFormatConsole изменение формата вывода в консоль по конкретному уровню
+func (l *Logger) SetLevelFormatConsole(level levelKey, format string) {
 	if lv, ok := l.logs[level]; ok {
 		lv.format = format
 	} else {
@@ -171,8 +183,8 @@ func (l *Logger) SetLevelFormatConsole(level, format string) {
 	}
 }
 
-//SetLevelFormatFile изменение формата вывода в фаил
-func (l *Logger) SetLevelFormatFile(level, format string) {
+//SetLevelFormatFile изменение формата вывода в фаил по конкретному уровню
+func (l *Logger) SetLevelFormatFile(level levelKey, format string) {
 	if lv, ok := l.logs[level]; ok {
 		lv.formatFile = format
 	} else {
@@ -180,8 +192,8 @@ func (l *Logger) SetLevelFormatFile(level, format string) {
 	}
 }
 
-//SetLevelFile изменение файла вывода
-func (l *Logger) SetLevelFile(level string, file *os.File) {
+//SetLevelFile изменение файла вывода по конкретному уровню
+func (l *Logger) SetLevelFile(level levelKey, file *os.File) {
 	if lv, ok := l.logs[level]; ok {
 		lv.file = file
 	} else {
@@ -189,8 +201,8 @@ func (l *Logger) SetLevelFile(level string, file *os.File) {
 	}
 }
 
-//SetLevelColor изменение цвета сообщения
-func (l *Logger) SetLevelColor(level string, color int) {
+//SetLevelColor изменение цвета сообщения по конкретному уровню
+func (l *Logger) SetLevelColor(level levelKey, color int) {
 	if lv, ok := l.logs[level]; ok {
 		lv.color = color
 	} else {
@@ -198,8 +210,8 @@ func (l *Logger) SetLevelColor(level string, color int) {
 	}
 }
 
-//SetLevelBackColor изменение цвета фона сообщения
-func (l *Logger) SetLevelBackColor(level string, color int) {
+//SetLevelBackColor изменение цвета фона сообщения по конкретному уровню
+func (l *Logger) SetLevelBackColor(level levelKey, color int) {
 	if lv, ok := l.logs[level]; ok {
 		lv.backColor = color
 	} else {
@@ -207,32 +219,33 @@ func (l *Logger) SetLevelBackColor(level string, color int) {
 	}
 }
 
-//SetFormatConsole изменение формата вывода в консоль
+//SetFormatConsole изменение формата вывода в консоль для всех уровней
 func (l *Logger) SetFormatConsole(format string) {
 	for _, v := range l.logs {
 		v.format = format
 	}
 }
 
-//SetFormatFile изменение формата вывода в фаил
+//SetFormatFile изменение формата вывода в фаил  для всех уровней
 func (l *Logger) SetFormatFile(format string) {
 	for _, v := range l.logs {
 		v.formatFile = format
 	}
 }
 
-//SetFile изменение файла вывода
+//SetFile изменение файла вывода  для всех уровней
 func (l *Logger) SetFile(file *os.File) {
 	for _, v := range l.logs {
 		v.file = file
 	}
 }
 
+//initForm парсит основное форматирование уровня, заполняя управляющими байтами
 func (lv *level) initForm(format string) string {
 	color := "\033[38;5;" + fmt.Sprint(lv.color) + "m"
 	colorF := "\033[48;5;" + fmt.Sprint(lv.backColor) + "m"
 	format = strings.ReplaceAll(format, "%{message}", "%v")
-	format = strings.ReplaceAll(format, "%{level}", lv.level)
+	format = strings.ReplaceAll(format, "%{level}", string(lv.level))
 	format = strings.ReplaceAll(format, "%{color}", color)
 	format = strings.ReplaceAll(format, "%{color:f}", colorF)
 	format = strings.ReplaceAll(format, "%{color:rf}", "\033[49m")
@@ -242,6 +255,7 @@ func (lv *level) initForm(format string) string {
 	return format
 }
 
+//parser парсит форматирование сообщения, заполняя необходимыми данными
 func (lv *level) parser(format string) string {
 	regex := regexp.MustCompile("%{time[^{}]+")
 	dateFind := regex.FindString(format)
